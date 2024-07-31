a6721892f4090dc978cfd77f5055a3346caf698b
Ic4ro <161033248+Ic4ro@users.noreply.github.com>
2024-07-31 11:38:20 -0300

    Add files via upload

---
+++ Exercicio01.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+     Scanner teclado = new Scanner( System.in);
+     
+     // sout
+     System.out.println("Digite seu nome: ");
+     String nome = teclado.next();
+     
+     System.out.println("Digite sua idade: ");
+     int idade = teclado.nextInt();
+     
+     System.out.println("Digite seu peso: ");
+     double peso = teclado.nextDouble ();
+     
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+        
+        teclado.close();
+     
+        
+        
+    }
+}
